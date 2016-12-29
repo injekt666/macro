3abba1e10f7eb4dad61e6101ec2c457742a082d7
Michele Levorato <MicheleLevorato@users.noreply.github.com>
2016-12-29 16:26:01 +0100

    Create trova_uguali.vb

---
+++ trova_uguali.vb
@@ -0,0 +1,19 @@
+Sub trovaUguali()
+    
+    ' se i valori della colonna A sono presenti nella colonna B scrive una stringa in C
+    
+    Dim numRighe As Integer
+
+    numRighe = ActiveSheet.UsedRange.Rows.Count
+    
+    For x = 1 To numRighe
+        For y = 1 To numRighe
+            If Range("A" & i).Value = Range("B" & y).Value Then
+                Range("C" & i).Value = "trovato"
+            End If
+        Next y
+    Next x
+
+    MsgBox "Esecuzione terminata"
+
+End Sub
